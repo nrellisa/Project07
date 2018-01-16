ed735f1b160a9cb8324cb25acb669a0e1eb2f9e3
Acer_User <Acer_User@LAPTOP-FDTKD2LB>
2018-01-17 00:47:33 +0800

    Project07

---
+++ src/Test1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Acer_User
+ */
+public class Test1 {
+    
+public static void main(String args[]) {
+    int x = 30;
+    
+    if( x < 20){
+        System.out.println("This is if statement");
+    }else {
+        System.out.println("This is else statement");
+    }
+}
+}
